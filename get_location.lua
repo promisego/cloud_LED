008653a9102568b205762f74c6041b31515faf91
Promise_ <1340823522@qq.com>
2018-04-02 22:32:42 +0800

    2018/4/2

--- get_location.lua
+++ get_location.lua
@@ -1,16 +1,18 @@
 --定义全局变量
-Http_result = 0
+Http_result = nil
+string_result=""
 --定义获取数据的函数
+
 function Get_location_city()
-    local url="http://int.dpool.sina.com.cn/iplookup/iplookup.php"
+    local url="http://int.dpool.sina.com.cn/iplookup/iplookup.php?"
     local wifi_status=wifi.sta.status()
     http.get(url,nil,function(code,data)
-        Http_result = 0
+        Http_result = nil
         if (code<0) then
             print("HTTP fail")
         else
-            print(data)
-            Http_result = 1
+            string_result = string.rep(data,1)
+            Http_result = true
         end
         end)
 end
@@ -19,7 +21,14 @@ end
 
 tmr.alarm(2,1000,tmr.ALARM_AUTO,function()
     Get_location_city()
-    if(Http_result ~= 0) then
+    if(Http_result ~= nil) then
         tmr.stop(2)
+        print(string_result)
+        print(string.find(string_result , "中"))
+        print(string.find(string_result,[^\u4e00-\u9fa5],9))
+        local param1 ,_= string.find(string_result,"\S")
+        local str = string.sub(string_result,1,param1)
+        str = string.reverse(str)
+        print(param1)
     end
     end)
--- init.lua
+++ init.lua
@@ -72,15 +72,14 @@ if(KEY_Status == 0) then
 else
     wifi_wait = 0
     tmr.alarm(1,1000,tmr.ALARM_AUTO,function()
-        --if (wifi.sta.getip() == nil) then
-            --wifi_wait = wifi_wait + 1
-            --if (wifi_wait > 10) then
-               --wifi_wait = 0
-            --end
-        --else
-            --tmr.stop(1)
-            --dofile("get_location.lua")
-       -- end
-       )
-
+        if (wifi.sta.getip() == nil) then
+            wifi_wait = wifi_wait + 1
+            if (wifi_wait > 10) then
+               wifi_wait = 0
+            end
+        else
+            tmr.stop(1)
+            dofile("get_location.lua")
+        end
+    end)
 end
