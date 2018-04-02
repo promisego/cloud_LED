30a8cff779cbfa5cf9267cc46eb3873519f2c5e5
Pomise <1340823522@qq.com>
2018-04-03 03:16:59 +0800

    four

--- get_location.lua
+++ get_location.lua
@@ -1,7 +1,19 @@
 --定义全局变量
 Http_result = nil
+Weather_result = nil
 string_result=""
+cityname_head = 0
+cityname_tail = 0
+str_city = ''
 --定义获取数据的函数
+function Get_ip()
+    local url = "http://ip.qq.com/cgi-bin/index"
+    http.get(url,nil,function(code,data)
+        if(code>0) then
+        print(code,data)
+        end
+        end)
+end
 
 function Get_location_city()
     local url="http://int.dpool.sina.com.cn/iplookup/iplookup.php?"
@@ -17,18 +29,60 @@ function Get_location_city()
         end)
 end
 
---调用函数
+function Get_cityname(res)
+    local str_len = string.len(res)
+    local str = string.byte(res,str_len,str_len)
+    local judge_flag = nil
+    cityname_head = 0
+    cityname_tail = 0
+    for i = str_len,1,-1 do
+        if(judge_flag == nil and string.byte(res,i,i) ~= str) then
+                cityname_tail = i
+                judge_flag = true
+        end
+        if(judge_flag ~= nil and string.byte(res,i,i) == str) then
+            cityname_head = i+1
+            break
+        end
+   end
+end
 
-tmr.alarm(2,1000,tmr.ALARM_AUTO,function()
-    Get_location_city()
+local function urlEncode(s)  
+     s = string.gsub(s, "([^%w%.%- ])", function(c) return string.format("%%%02X", string.byte(c)) end)  
+    return string.gsub(s, " ", "+")  
+end  
+
+function Get_weather(city_name)
+    local url = 'https://query.yahooapis.com/v1/public/yql?q=select * from geo.placefinder where text="39.9919336,116.3404132" and gflags = "R"'
+    --url = url..'%E5%8D%97%E6%98%8C'
+    --url = url..'%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+     http.get(url,nil,function(code,data)
+        if(code>0) then
+            if(string.find(data,'Yahoo')) then
+                Weather_result = true
+            end
+        end
+        end)
+end
+
+--调用函数
+       
+tmr.alarm(3,2000,tmr.ALARM_AUTO,function()
+    Get_location_city(str_city)
     if(Http_result ~= nil) then
-        tmr.stop(2)
-        print(string_result)
-        print(string.find(string_result , "中"))
-        print(string.find(string_result,[^\u4e00-\u9fa5],9))
-        local param1 ,_= string.find(string_result,"\S")
-        local str = string.sub(string_result,1,param1)
-        str = string.reverse(str)
-        print(param1)
+        Http_result = nil
+        tmr.stop(3)
+        Get_cityname(string_result)
+        str_city=string.sub(string_result,cityname_head,cityname_tail)
+        print(str_city)
+        collectgarbage()
+        tmr.alarm(2,5000,tmr.ALARM_AUTO,function()
+            Get_weather(str_city)
+            collectgarbage()
+            if(Weather_result ~= nil) then
+            print(Weather_result)
+            tmr.stop(2)
+        end
+        end)
     end
     end)
